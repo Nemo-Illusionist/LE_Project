09b9e03d9f567e45176d6e33cb5069948e1b13f3
ThePaBlike <thepablike@gmail.com>
2017-08-04 14:13:57 +0300

    Add project

---
+++ LE_Project/LocalizationEditor/APIScripts/LELanguages.cs
@@ -0,0 +1,61 @@
+﻿namespace LocalizationEditor
+{
+    public class LELanguages
+    {
+        public static string Urdu = "ur";
+        public static string Arabic = "ar";
+        public static string Afrikaans = "af";
+        public static string Bulgarian = "bg";
+        public static string Belarusian = "be";
+        public static string Catalan = "ca";
+        public static string Czech = "cs";
+        public static string Danish = "da";
+        public static string German = "de";
+        public static string Greek = "el";
+        public static string English = "en";
+        public static string Spanish = "es";
+        public static string Estonian = "et";
+        public static string Basque = "eu";
+        public static string Finnish = "fi";
+        public static string Galician = "gl";
+        public static string Gujarati = "gu";
+        public static string Hebrew = "he";
+        public static string Hindi = "hi";
+        public static string Croatian = "hr";
+        public static string Hungarian = "hu";
+        public static string Armenian = "hy";
+        public static string Indonesian = "id";
+        public static string Icelandic = "is";
+        public static string Italian = "it";
+        public static string Japanese = "ja";
+        public static string Georgian = "ka";
+        public static string Persian = "fa";
+        public static string Kannada = "kn";
+        public static string Korean = "ko";
+        public static string Konkani = "kok";
+        public static string Faroese = "fo";
+        public static string French = "fr";
+        public static string Lithuanian = "lt";
+        public static string Latvian = "lv";
+        public static string Macedonian = "mk";
+        public static string Marathi = "mr";
+        public static string Dutch = "nl";
+        public static string Norwegian = "no";
+        public static string Polish = "pl";
+        public static string Portuguese = "pt";
+        public static string Romanian = "ro";
+        public static string Russian = "ru";
+        public static string Slovak = "sk";
+        public static string Albanian = "sq";
+        public static string Swedish = "sv";
+        public static string Kiswahili = "sw";
+        public static string Slovenian = "sl";
+        public static string Tamil = "ta";
+        public static string Telugu = "te";
+        public static string Thai = "th";
+        public static string Turkish = "tr";
+        public static string Ukrainian = "uk";
+        public static string Vietnamese = "vi";
+        public static string Chinese = "zh";
+    }
+}
